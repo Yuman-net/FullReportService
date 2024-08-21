31c1364d1a6c3e296ab97b562397a82dcbb14bfe
Andrei Nikolaev <andrei.nikolaev@cmwp.ru>
2024-08-21 20:41:22 +0300

    Подключени Stylecop. Добавлена первая сущность для проверки

---
+++ Services/ReportService.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ReportService.cs" company="Andrey Nikolaev">
+// Copyright (c) Andrey Nikolaev. All rights reserved.
+// </copyright>
+
+namespace Services
+{
+    /// <summary>
+    /// Сервис по работе с отчетами.
+    /// </summary>
+    public class ReportService
+    {
+    }
+}
---
+++ WebApi/Controllers/ReportController.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ReportController.cs" company="Andrey Nikolaev">
+// Copyright (c) Andrey Nikolaev. All rights reserved.
+// </copyright>
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    /// Контроллер по работе с отчетом.
+    /// </summary>
+    public sealed class ReportController
+    {
+    }
+}
